3a4314f92900383d420e6fc347e02708f136ac6c
kun <kunfeng_wangcs@163.com>
2024-01-17 12:55:22 +0800

    基于Redis实现短信登录

---
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,36 @@
+package com.hmdp.config;
+
+import com.hmdp.interceptor.LoginInterceptor;
+import com.hmdp.interceptor.RefreshToTokerInterceptor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+public class MvcConfig implements WebMvcConfigurer {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //登录拦截
+        registry.addInterceptor(new LoginInterceptor()).
+                excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+
+        //token刷新的拦截器
+        registry.addInterceptor(
+                new RefreshToTokerInterceptor(stringRedisTemplate))
+                .addPathPatterns("/**").order(0);
+
+    }
+}
---
+++ src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmdp.interceptor;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.utils.UserHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 登录拦截
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1,获取session
+        HttpSession session = request.getSession();
+        //2,获取session中的用户
+        Object user = session.getAttribute("user");
+        //3，判断用户是否存在
+        if (user == null){
+            //4,不存在拦截，返回401(无权限)状态码
+            response.setStatus(401);
+        }
+        //5，存在，保存用户信息到ThreadLocal
+        UserHolder.saveUser((UserDTO) user);
+        //6,放行
+        return true;
+    }
+}
+
