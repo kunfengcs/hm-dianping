2715ca5dee79fa3deb10853be82d03db0b91e855
kun <kunfeng_wangcs@163.com>
2024-02-05 17:05:31 +0800

    乐观锁解决超卖问题

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -53,7 +53,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         //5，扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).gt("stock",0).update();//where id = ? and stock > 0
         if (!success) {
             //扣减库存
             return Result.fail("库存不足");
