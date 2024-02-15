4b49f8a41b27d428e5967d0fc1ac69322e3edcf3
kun <kunfeng_wangcs@163.com>
2024-02-15 23:38:10 +0800

    分布式锁实现 v3.0 通过执行Lua脚本来保证解锁操作的原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,13 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -22,6 +26,17 @@ public class SimpleRedisLock implements ILock{
      */
     private static final String ID_PREFIX = UUID.randomUUID().toString(true)+ "-";
 
+    /**
+     * 解锁Lua脚本，默认RedisScript实例，用于原子性的解锁操作
+     */
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    //静态块，加载并设置解释Lua脚本
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));//设置Lua脚本路径
+        UNLOCK_SCRIPT.setResultType(Long.class);//设置返回结果类型为Long
+    }
     private static final String KEY_PREFIX = "lock:";
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -34,16 +49,37 @@ public class SimpleRedisLock implements ILock{
         return Boolean.TRUE.equals(success);
     }
 
+
+    /**
+     * 释放锁
+     * 通过执行Lua脚本来保证解锁操作的原子性
+     */
     @Override
-    public void unlock() {
-        //获取线程标识
+    public void unlock(){
+        // 获取当前线程的唯一标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
-        //获取锁中的标识
+        //调用Lua脚本进行解锁操作
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),//参数列表：锁的key
+                threadId // 参数： 当前线程的唯一标识
+        );
+    }
+    /**
+     * **注释已移除的方法：**
+     *
+     * 上述方法是一个非原子性的解锁方式，这里为了实现原子性解锁，采用了lua脚本的方式，因此注释掉了这个原始的unlock方法。
+     */
+    /*@Override
+    public void unlock() {
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标示
         String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        //判断标识是否一致
-        if (threadId.equals(id)) {
-            //释放锁
+        // 判断标示是否一致
+        if(threadId.equals(id)) {
+            // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
