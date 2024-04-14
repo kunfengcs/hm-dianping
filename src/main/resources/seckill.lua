7e14eab8c6aaa305b2e6bdf0bb5dfb43ba57fc04
“王坤峰” <kunfeng_wangcs@163.com>
2024-04-14 23:41:08 +0800

    【秒杀优化】秒杀资格判断——lua脚本

---
+++ src/main/resources/seckill.lua
@@ -0,0 +1,31 @@
+-- 1.参数列表
+-- 1.1 优惠券id
+local voucherId = ARGV[1]
+--1.2 用户id
+local userId = ARGV[2]
+-- 1.3 订单id
+local orderId = ARGV[3]
+
+-- 2. 数据key
+-- 2.1 库存key
+local stockKey = 'seckill:stock' .. voucherId
+--2.2 订单key
+local orderKey = 'seckill:order' .. voucherId
+
+-- 3 业务脚本
+-- 3.1 判断库存是否充足 get stockKey
+if(tonumber(redis.call('get',stockKey)) <= 0) then
+    -- 3.1.1 库存不足，返回1
+    return 1
+end
+
+-- 3.2 判断用户是否下单 SISMEMBER orderKey userId
+if(redis.call('SISMEMBER',orderKey,userId) == 1) then
+    -- 3.2.1 存在，说明是重复下单，返回2
+    return 2
+end
+
+-- 3.4 扣库存 incrby stockKey -1
+redis.call('incrby',stock,-1)
+-- 3.5 下单（保存） sadd orderKey userId
+redis.call('sadd',orderKey,userId)
