338e4b9079b62368f7aba8de1c98398073d31929
“王坤峰” <kunfeng_wangcs@163.com>
2024-04-18 18:25:11 +0800

    修改  RedissonConfig文件，字段注入redisson配置

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -3,18 +3,30 @@ package com.hmdp.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password:#{null}}")
+    private String redisPassword;
+
     @Bean
     public RedissonClient redissonClient(){
+
         //配置
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
-//        config.useSingleServer().setAddress("redis://192.168.111.129").setPassword("123456");
+        config.useSingleServer().setAddress("redis://"+redisHost+":"+redisPort).setPassword(redisPassword);
         //创建RedissonClient对象
         return Redisson.create(config);
     }
